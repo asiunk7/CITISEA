8afa4ae3be38651b6b40805e4dc575d3e7ecc89e
asiunk7 <104195267+asiunk7@users.noreply.github.com>
2025-05-21 12:16:54 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('citisea-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/CITISEA.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
